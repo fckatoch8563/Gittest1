54b3a3f2a8c987e33bc664d96378001a3abc4945
fckatoch8563 <fckatoch45@gmail.com>
2018-08-30 08:35:10 +0530

    Uploaded Swift file

---
+++ Code1.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+add()
+    return y
+}
+returnFifteen()
